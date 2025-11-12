ee9cb98294b5659d398a38a5d31b61b308aa2e5f
closedAquarius <2383731319@qq.com>
2025-11-12 23:13:25 +0800

    初始化java项目

---
+++ Java/expr1_java/src/main/java/org/csu/expr1_java/Expr1JavaApplication.java
@@ -0,0 +1,13 @@
+package org.csu.expr1_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Expr1JavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Expr1JavaApplication.class, args);
+    }
+
+}
---
+++ Java/expr1_java/src/test/java/org/csu/expr1_java/Expr1JavaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.csu.expr1_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Expr1JavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
